d832363c4355fd9d9c9f382651fb5c5ed476a829
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-24 03:23:54 +0000

    setup_mysql_test.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---  creates the database hbtn_0c_0 in your MySQL server.
+--  creates the database 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
