16c61ca7040d694961b80ccd32662aef584df490
Cu7ious <328@holbertonschool.com>
2018-07-26 00:03:53 +0000

    Added one-to-many relationship with the class Place

--- models/user.py
+++ models/user.py
@@ -4,6 +4,7 @@
 '''
 from models import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -16,3 +17,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user",
+                          cascade="all, delete-orphan")
